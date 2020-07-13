04868029ab62a68a436bcb241bcaea87e1a9871b
MrEzikSuslikovich <56349926+MrEzikSuslikovich@users.noreply.github.com>
2020-07-13 14:32:20 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+	<header class="header">
+		<div class="wrapper">
+			<div class="header__wrapper">
+				<div class="header__logo">
+					<a href="/" class="header__logo-link">
+						<img src=".\img\svg\Logo.PNG" class="header__logo-pic">
+					</a>
+				</div>
+				<nav class="header__nav">
+					<ul class="header__list">
+						<li class="header__item">
+							<a class="header__link" href="#">Why Alivio</a>
+						</li>
+						<li class="header__item">
+							<a class="header__link" href="#">Solutions</a>
+						</li>
+						<li class="header__item">
+							<a class="header__link" href="#">Community</a>
+						</li>
+						<li class="header__item">
+							<a class="header__link" href="#">Pricing</a>
+						</li>
+					</ul>
+				</nav>
+				<div class="header__login">
+					<a  class="header__link" href="#">Sign in</a>
+					<button class="header__button">Start Trial</button>
+				</div>
+			</div>
+		</div>
+	</header>
+	<main class="main">
+		<section class="intro">
+			<div class="wrapper">	
+				<h1 class="intro__title">
+					Be aware,
+					Manage well.
+				</h1>
+				<p class="intro__text">
+					Everyone experiences stress in different ways.
+					Let Alivio guide you, in a personalized journal experience, to overcome your stress.
+				</p>
+				<button class="main__button">Find your way</button>
+			</div>	
+		</section>
+		<section class="howitworks">
+			<div class="wrapper">
+				<p class="howitworks__text">
+					How it works
+				</p>
+				<div class="header__wrapper">
+					<h1 class="howitworks__title">
+						Understand & Release the stress in 3 steps
+					</h1>
+					<p class="howitworks__headlinetext">
+						Alivio offers as many journals it takes, tackling different areas such as anxiety, overwhelmedness, sadness, or anger, and a variety of personal causes, to help you become aware of your emotions, and guide you in how to manage stress.
+					</p>
+				</div>
+				<div class="howitworks_wrapp">
+					<div class="howitworks__wrap1">
+						<p class="howitworks__steps">
+							01
+							<p class="howitworks__step-name">Personalize</p>
+						</p>
+
+					<p class="howitworks__text">
+						Answer a quick survey about how you express yourself, what causes you stress, and what area would you like to work on. This way, we can fully personalize your journal!
+					</p>
+					<img class="howitworks__img" src=".\img\svg\Rectangle1.PNG">
+				</div>
+				<div class="howitworks__wrap2">
+					<p class="howitworks__steps">
+						02
+						<p class="howitworks__step-name">Write & Understand</p>
+					</p>
+					<p class="howitworks__text">
+						Write, draw, reflect, understand. Alivio will guide you through the prompts and will help you manage your stress!
+					</p>
+					<img class="howitworks__img" src=".\img\svg\Rectangle2.PNG">
+				</div>
+				<div class="howitworks__wrap3">
+					<p class="howitworks__steps">
+						03
+						<p class="howitworks__step-name">Alivio!</p>
+					</p>
+					<p class="howitworks__text">
+						Now you are aware, and have a way to manage and overcome your own stress.
+					What are you waiting for? Alivio today!
+					</p>
+					<img class="howitworks__img" src=".\img\svg\Rectangle3.PNG">
+				</div>
+			</div>
+			</div>
+		</section>
+		<section class="successstory">
+			<div class="wrapper">
+				<div class="successstory_wrapper">
+					<div class="successstory__text">
+						<h1 class="successstory__header">
+							Let's hear about Kayla's success story
+						</h1>
+						<p class="successstory__normaltext">
+							See how well Alivio works in a real customer’s life. 
+						</p>
+						<button class="main__button">
+							Let’s get started
+						</button>
+					</div>
+					<div class="successstory_video">
+						<iframe width="200%" height="120%" src="https://www.youtube.com/embed/k2WgWaByuL8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+
+		<section class="ourproduct">
+			<div class="product_wrap">
+				<img class="ourproduct__image" src="./img/svg/ourproduct.PNG">
+					<div class="productdescription">
+						<p>Our product</p>
+						<h1 class="howitworks__title">
+							You tell us your stress,
+							We make your diary
+						</h1>
+						<p class="header__link">
+							Everyone experiences it, and in different ways.
+							Let Alivio guide you, in a personalized journal experience, to overcome 	your stress.			
+						</p>
+						<button class="main__button">Find Your Way</button>
+					</div>
+			</div>
+			<div class="wrapper">
+			<div class="ourproduct__card">
+					<p class="ourproduct__cardheader">Customized For</p>
+					<img class="ourproduct__cardimg" src="./img/svg/cardimage.PNG" />
+					<p class="ourproduct__text">Mikayla</p>
+					<h1 class="ourproduct__header">
+						Your custom contents
+					</h1>
+					<ul class="ourproduct__list" >
+						<li class="ourproduct__listel">Understand Your Stress</li> 
+						<li class="ourproduct__listel">Anger Management</li> 
+					</ul>
+			</div>
+			</div>
+		</section>
+		
+
+		<footer class="footer">
+			<div class="footerwrap">
+				<div class="footer__header">Alivio</div>
+				<div class="footer__element">Why Alivio</div>
+				<div class="footer__element">Solutions</div>
+				<div class="footer__element">Community</div>
+				<div class="footer__element">Pricing</div>
+			</div>
+		</footer>
+
+
+	</main>
+</body>
+</html>
